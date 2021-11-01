6d1a42d6f7fab50aca37af4ef101d670144226df
Jose Victor Persset <jpersset@gmail.com>
2021-11-01 15:24:06 -0300

    Started layouting the Create User page

---
+++ src/pages/User/CreateUser.tsx
@@ -0,0 +1,49 @@
+import { FormEvent, useState } from "react";
+import api from "../../services/api";
+
+function CreateUser() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function handleCreateUser(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  return (
+    <div id="page-create-user">
+      <main>
+        <div className="main-content">
+          <form onSubmit={handleCreateUser}>
+            <label htmlFor="user-name">Nome: </label>
+            <input
+              type="text"
+              name="user-name"
+              id="user-name"
+              onChange={(event) => setName(event.target.value)}
+              value={name}
+            />
+            <label htmlFor="user-email">Email: </label>
+            <input
+              type="email"
+              name="user-email"
+              id="user-email"
+              onChange={(event) => setEmail(event.target.value)}
+              value={email}
+            />
+            <label htmlFor="user-password">Senha: </label>
+            <input
+              type="password"
+              name="user-password"
+              id="user-password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+            />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export { CreateUser };
